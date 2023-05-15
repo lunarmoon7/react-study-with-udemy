8e7efae89ecaae5c582c6d627944fe35738d1270
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-05-15 17:28:56 +0900

    자식 컴포넌트 재평가 자세히 살펴보기

--- ch/Section12/src/App.js
+++ ch/Section12/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import Button from './components/UI/Button/Button';
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
